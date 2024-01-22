e7315f5c7645a4123a79ae896fc90bfe0435fb58
Ivan Hrachyshkin <ivanhrachyshkin@gmail.com>
2024-01-22 22:53:15 +0100

    router refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Bar from './components/bar/Bar';
+import LoginForm from './components/content/LoginForm';
+import Profile from './components/content/Profile';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Bar />
+        <Outlet />
+      </>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <LoginForm />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
